900fbf7fed97c25816321c540eb65fc38c8ac631
Alex Sun <me@xsun.work>
2025-03-15 05:23:56 +1100

    refactor: remove unused profile and notification components

--- apps/web/app/(protected)/(dashboard)/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-import ProfileLandingPage from '@app-launch-kit/modules/user-profile/components/ProfileLandingPage';
-
-const Profile = () => {
-  return <ProfileLandingPage />;
-};
-
-export default Profile;
