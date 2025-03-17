309ebb44a2b4ce912543609acdda34d4dab1d92b
Alex Sun <me@xsun.work>
2025-03-18 01:26:30 +1100

    feat: change React Native to capacitor

---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'work.xsun.app',
+  appName: 'Talkify',
+  webDir: 'public'
+};
+
+export default config;
